b2f1d6aa7f26d25d737a61aa6941107a260f6eba
Eros <erosazul@gmail.com>
2017-09-19 12:12:48 +0200

    Iniciando repositorio

---
+++ Pruebas/6Kyu/Find_the_odd_int.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+
+namespace Pruebas
+{
+    public class Find_the_odd_int
+    {
+        public static int find_it(int[] seq)
+        {
+            ArrayList numeros = new ArrayList(seq);
+            while (numeros.Capacity > 1)
+            {
+                if (numeros.IndexOf(numeros[0], 1) != -1)
+                {
+                    numeros.Remove(numeros.IndexOf(numeros[0], 1));
+                    numeros.Remove(0);
+                }
+                else return (int)numeros[0];
+            }
+            return 0;
+        }
+    }
+}
---
+++ Pruebas/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Pruebas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string pruebas = Console.ReadLine();
+                if ("FIN".Equals(pruebas.ToUpper())) break;
+                string[] divi = pruebas.Split('-');
+                int[] num = new int[divi.Length];
+                for (int i = 0; i < divi.Length; i++) num[i] = int.Parse(divi[i]);
+
+                Console.WriteLine(Find_the_odd_int.find_it(num));
+
+            }
+        }
+    }
+}
