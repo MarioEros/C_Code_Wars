10b4d1582c13886a79a73bfe85c4c245342c19d3
Eros <erosazul@gmail.com>
2017-10-18 13:59:13 +0200

    Otro 7 Kyu

---
+++ Pruebas/7Kyu/Sum_of_a_sequence.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pruebas._7Kyu
+{
+    class Sum_of_a_sequence
+    {
+        public static int SequenceSum(int start, int end, int step)
+        {
+            int sum = 0;
+            for (int i = start; i <= end; i+=step) sum +=i;
+            return sum;
+        }
+    }
+}
