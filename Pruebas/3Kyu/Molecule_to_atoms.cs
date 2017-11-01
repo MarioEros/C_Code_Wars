2bb24261915f56381770659f9420935686fdd5b1
Eros <erosazul@gmail.com>
2017-11-01 13:30:31 +0100

    OutOfTime Atomos

--- Pruebas/3Kyu/Molecule_to_atoms.cs
+++ Pruebas/3Kyu/Molecule_to_atoms.cs
@@ -11,50 +11,84 @@ namespace Pruebas
     {
         public static Dictionary<string, int> ParseMolecule(string formula)
         {
-            //UubFe(Al(P(Mg(NBeArLiNKBe)3MgOUuu(OSiCaLiULi)4(MgUtnFSArUup)8)13((FeFePSi)3Uuo(SUAr)12)9(LiClUbn(UuoUUub)8(UqnAlUPUuoNa)8(UupArUtnClFeN)5(BeUubK)5)6((HUuuCa)2ArUqnUup)12(F(NBeUunBe)11(HCBe)12)6O)8((P(UuuUqnNUuu)2(UubPUbnFeUtnUtn)12(UbnBeMgArNaH)4K(UuuUunUUUubNUup)13)12P(Al(MgSiUuuFUtnBeHe)7(UubUunUuoNeNK)8U(UqnUuoOUbn)9HBe)12)2C)8Uuo(((NLiLiAlSiUun)8SiUP((NaBeBSUun)2Uup(UbnUKSiH)7)6((CUuoClNUupK)2(ClLiNeHArSiHe)11HHe(MgNaUuoSAr)7)7)3UubMgMg((BeC(UuuCArMg)10Si)12((UtnUtnKFeNe)2(HUtnUunPFeKCa)13Ar)10F)6(ClFF(Be(UunFOCaUbn)5Uqn)5FBCl)3(((HUBeNa)4Mg(PHeUup)5(AlNeMgHUuo)13)12KOUubUtnK)5)10BMg
-            //System.IndexOutOfRangeException : Index was outside the bounds of the array.
-            //C6H12O6       Expected: < [C, 6], [H, 12], [O, 6] >
-            //              But was:  < [O, 6], [H, 492], [C, 6] >
-            //(C5H5)Fe(CO)2CH3 da fallo porque aplica el exp del lado derecho a ambos parentesis
-            //((H)2[O])
             Dictionary<string, int> diccio = new Dictionary<string, int>();
-            List<String> lista = new List<string>(Regex.Split(formula, @"([\{\[\(][A-Za-z0-9]*[\}\]\)])"));
-            lista.RemoveAll(a => a == " "|| a== "");
-            //(C5H5)    Fe  (CO)    2CH3
-            while (lista.Count > 1)
+            while (formula.IndexOfAny(new char[] { '(', '[', '{' }) != -1)
             {
+                List<String> lista = new List<string>(Regex.Split(formula, @"([\{\[\(][A-Za-z0-9]*[\}\]\)])"));
+                lista.RemoveAll(a => a == " " || a == "");
                 int exponente = 1;
-                for (int i = 0; i > lista.Count; i++)
+                for (int i = 0; i < lista.Count; i++)
                 {
-                    //(C5H5),(CO)
-                    if ((lista[i].StartsWith("(") && lista[i].EndsWith(")"))||(lista[i].StartsWith("[") && lista[i].EndsWith("]"))||(lista[i].StartsWith("{") && lista[i].EndsWith("}")))
+                    if ((lista[i].StartsWith("(") && lista[i].EndsWith(")")) || (lista[i].StartsWith("[") && lista[i].EndsWith("]")) || (lista[i].StartsWith("{") && lista[i].EndsWith("}")))
                     {
-                        if(i+1<lista.Count)exponente = encuentraNum(lista[i + 1]);
+                        if (i + 1 < lista.Count)
+                        {
+                            exponente = encuentraNum(lista[i + 1]);
+                            lista[i + 1] = (exponente > 9 ? lista[i + 1].Substring(2) : exponente > 1 ? lista[i + 1].Substring(1) : lista[i + 1]);
+                        }
                         List<String> Atomos = new List<string>(Regex.Split(lista[i], @"([A-Z][a-z]{0,2}[0-9]*)"));
                         Atomos.RemoveAll(a => a == "{" || a == "(" || a == "[" || a == " " || a == "" || a == ")" || a == "]" || a == "}");
-                        //C5    H5
+                        if (exponente != 1)
+                        {
+                            for (int j = 0; j < Atomos.Count; j++)
+                            {
+                                List<String> At = new List<String>(Regex.Split(Atomos[j], @"([0-9]+)"));
+                                At.RemoveAll(b => b == " " || b == "");
+                                Atomos[j] = At[0] + (At.Count > 1 ? "" + (int.Parse(At[1]) * exponente) : "" + exponente);
+                            }
+                        }
+                        if (i == 0)
+                        {
+                            lista[0] = "";
+                            Atomos.ForEach(a => lista[0] += a);
+                            if (1 < lista.Count)
+                            {
+                                lista[0] += lista[1];
+                                lista.RemoveAt(1);
+                                i--;
+                            }
+                        }
+                        else
+                        {
+                            Atomos.ForEach(a => lista[i - 1] += a);
+                            lista.RemoveAt(i);
+                            if (i < lista.Count)
+                            {
+                                lista[i - 1] += lista[i];
+                                lista.RemoveAt(i);
+                                i--;
+                            }
+                            i--;
+                        }
                     }
                 }
+                formula = lista[0];
+            }
+            List<String> AtSplit = new List<string>(Regex.Split(formula, "([A-Z][a-z]*[0-9]*)"));
+            AtSplit.RemoveAll(a => a == "" || a == " ");
+            foreach (String item in AtSplit)
+            {
+                diccio.AddOrSum(item);
             }
             return diccio;
         }
         public static int encuentraNum(string num)
         {
-            for (int i = 0; i < num.Length; i++)
-            {
-                if (!char.IsDigit(num[i])) break;
-                else if (i + 1 < num.Length && char.IsDigit(num[i + 1])) return int.Parse(num.Substring(i, 2));
-                else return int.Parse(num.Substring(i, 1));
-            }
-            return 1;
+            int i = 0;
+            while (i < num.Length && Char.IsDigit(num[i])) i++;
+            return (i == 0 ? 1 : int.Parse(num.Substring(0, i)));
         }
     }
     public static class Dico
     {
-        public static void AddOrSum(this Dictionary<string, int> dic, string Ele, int ind)
+        public static void AddOrSum(this Dictionary<string, int> dic, string At)
         {
-            if (dic.ContainsKey(Ele)) dic[Ele] = dic[Ele] + ind;
-            else dic.Add(Ele, ind);
+            List<String> Frac = new List<String>(Regex.Split(At, @"([0-9]+)"));
+            Frac.RemoveAll(b => b == " " || b == "");
+            string key = Frac[0];
+            int value = (Frac.Count > 1 ? int.Parse(Frac[1]) : 1);
+            if (dic.ContainsKey(key)) dic[key] += value;
+            else dic.Add(key, value);
         }
     }
 }
--- Pruebas/Program.cs
+++ Pruebas/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
+using System.IO;
 
 namespace Pruebas
 {
@@ -8,44 +9,99 @@ namespace Pruebas
     {
         static void Main(string[] args)
         {
-            while (true)
+            FileStream fs = new FileStream(@"C:\Users\Eros\source\repos\C_Code_Wars\Pruebas\TextoPruebas.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            //StreamWriter sw= new StreamWriter(fs);
+            //sw.WriteLine("hola caracola");
+            //sw.Close();
+            StreamReader sr = new StreamReader(fs);
+            for (int m = 0;m<5;m++)
             {
-                string pruebas = Console.ReadLine();
-                if ("FIN".Equals(pruebas.ToUpper())) break;
+                FileStream fss = new FileStream(@"Pruebas.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+                StreamWriter sw = new StreamWriter(fss);
+                string pruebas = sr.ReadLine();
+                sw.WriteLine(pruebas);
+                //if ("FIN".Equals(pruebas.ToUpper())) break;
                 //string[] divi = pruebas.Split('*');
                 //int[] num = new int[divi.Length];
                 //for (int i = 0; i < divi.Length; i++) num[i] = int.Parse(divi[i]);
-
-                //List<String> lista = new List<string>(Regex.Split(pruebas, @"([\{\[\(][A-Za-z0-9]*[\}\]\)])")); List<String> lista2 = new List<string>();
-                /*List<String> lista = new List<string>(Regex.Split(pruebas, @"([\{\[\(][A-Za-z0-9]*[\}\]\)])"));
-                for (int i =0; i < lista.Count;i++)
+                Dictionary<string, int> diccio = new Dictionary<string, int>();
+                //(C5H5)    Fe  (CO)    2CH3
+                Console.WriteLine(pruebas);
+                while (pruebas.IndexOfAny(new char[] { '(', '[', '{' }) != -1)
                 {
-                    Console.WriteLine("L: "+lista[i]);
-                    Console.WriteLine("-->"+(i+1<lista.Count?encuentraNum(lista[i+1]):1));
-                }*/
-                /*Console.WriteLine("--------------------------");
-                lista.RemoveAll(a => a == "{" || a == "(" || a == "[" || a == " " || a == ")" || a == "]" || a == "}");
-                foreach (string item in lista)
-                {
-                    Console.WriteLine(item);
-                }*/
-                List<String> lista = new List<string>(Regex.Split(pruebas, @"([\{\[\(][A-Za-z0-9]*[\}\]\)])"));
-                lista.RemoveAll(a => a == " " || a == "");
-                foreach (string item in lista)
-                {
-                    Console.WriteLine(item);
+                    List<String> lista = new List<string>(Regex.Split(pruebas, @"([\{\[\(][A-Za-z0-9]*[\}\]\)])"));
+                    lista.RemoveAll(a => a == " " || a == "");
+                    int exponente = 1;
+                    for (int i = 0; i < lista.Count; i++)
+                    {
+                        //(C5H5),(CO)
+                        if ((lista[i].StartsWith("(") && lista[i].EndsWith(")")) || (lista[i].StartsWith("[") && lista[i].EndsWith("]")) || (lista[i].StartsWith("{") && lista[i].EndsWith("}")))
+                        {
+                            if (i + 1 < lista.Count)
+                            {
+                                exponente = encuentraNum(lista[i + 1]);
+                                lista[i + 1] = (exponente > 9 ? lista[i + 1].Substring(2) : exponente > 1 ? lista[i + 1].Substring(1) : lista[i + 1]);
+                            }
+                            List<String> Atomos = new List<string>(Regex.Split(lista[i], @"([A-Z][a-z]{0,2}[0-9]*)"));
+                            Atomos.RemoveAll(a => a == "{" || a == "(" || a == "[" || a == " " || a == "" || a == ")" || a == "]" || a == "}");
+                            //C5    H5
+                            if (exponente != 1)
+                            {
+                                for (int j = 0; j < Atomos.Count; j++)
+                                {
+                                    List<String> At = new List<String>(Regex.Split(Atomos[j], @"([0-9]+)"));
+                                    At.RemoveAll(b => b == " " || b == "");
+                                    Atomos[j] = At[0] + (At.Count > 1 ? "" + (int.Parse(At[1]) * exponente) : "" + exponente);
+                                }
+                            }
+                            if (i == 0)
+                            {
+                                lista[0] = "";
+                                Atomos.ForEach(a => lista[0] += a);
+                                if (1 < lista.Count)
+                                {
+                                    lista[0] += lista[1];
+                                    lista.RemoveAt(1);
+                                    i--;
+                                }
+                            }
+                            else
+                            {
+                                Atomos.ForEach(a => lista[i - 1] += a);
+                                lista.RemoveAt(i);
+                                if (i < lista.Count)
+                                {
+                                    lista[i - 1] += lista[i];
+                                    lista.RemoveAt(i);
+                                    i--;
+                                }
+                                i--;
+                            }
+                        }
+                    }
+                    pruebas = lista[0];
                 }
+                List<String> AtSplit = new List<string>(Regex.Split(pruebas, "([A-Z][a-z]*[0-9]*)"));
+                AtSplit.RemoveAll(a => a == "" || a == " ");
+                foreach (String item in AtSplit)
+                {
+                    diccio.AddOrSum(item);
+                }
+                foreach (var item in diccio)
+                {
+                    Console.WriteLine(item.Key+"-->"+item.Value);
+                }
+                Console.WriteLine("-------------------------------------");
             }
+            Console.ReadKey();
+            sr.Close();
+            fs.Close();
         }
         public static int encuentraNum(string num)
         {
-            for(int i = 0; i < num.Length; i++)
-            {
-                if (!char.IsDigit(num[i])) break;
-                else if (i + 1 < num.Length && char.IsDigit(num[i+1])) return int.Parse(num.Substring(i, 2));
-                else return int.Parse(num.Substring(i, 1));
-            }
-            return 1;
+            int i = 0;
+            while (i<num.Length&&Char.IsDigit(num[i])) i++;
+            return (i==0?1:int.Parse(num.Substring(0,i)));
         }
     }
 }
