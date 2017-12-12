e05c8e8568c04e46d1b06226aecea43daed33de5
Eros <erosazul@gmail.com>
2017-12-12 13:45:14 +0100

    Digital root

---
+++ Pruebas/6Kyu/Digital_Root.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pruebas
+{
+    class Digital_Root
+    {
+        public int DigitalRoot(long n)
+        {
+            string sol = "";
+            string auxiliar = n.ToString();
+            while (auxiliar.Length != 1)
+            {
+                auxiliar = auxiliar.Sum(a => int.Parse(a.ToString())).ToString();
+            }
+            return int.Parse(auxiliar);
+        }
+    }
+}
