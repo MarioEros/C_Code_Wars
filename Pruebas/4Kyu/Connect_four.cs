5d58d06ec1c626815fe93c1a66c982e6cadae622
MarioEros <erosazul@gmail.com>
2018-01-08 14:09:55 +0100

    Conecta 4

---
+++ Pruebas/4Kyu/Connect_four.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pruebas._4Kyu
+{
+    class Connect_four
+    {
+        public static string WhoIsWinner(List<string> piecesPositionList)
+        {
+            
+            return "hola";
+        }
+    }
+}
