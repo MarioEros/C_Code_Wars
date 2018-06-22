a671f510d3821d9d3eff4c0006f570558be30595
Mario Eros Rodriguez Espinosa <erosazul@gmail.com>
2018-06-22 10:11:25 +0200

    Update PatternCraft_Adapter.cs

--- Pruebas/7Kyu/PatternCraft_Adapter.cs
+++ Pruebas/7Kyu/PatternCraft_Adapter.cs
@@ -8,45 +8,15 @@ namespace Pruebas._7Kyu
 {
     class PatternCraft_Adapter
     {
-        public class Target
-        {
-            public int Health { get; set; }
-        }
-        public interface IUnit
-        {
-            void Attack(Target target);
-        }
-
-        public class Marine : IUnit
-        {
-            public void Attack(Target target)
-            {
-                target.Health -= 6;
+        public class MarioAdapter : IUnit{
+            private Mario mario;
+    
+            public MarioAdapter(Mario mario){
+                this.mario = mario;
             }
-        }
 
-        public class Zealot : IUnit
-        {
-            public void Attack(Target target)
-            {
-                target.Health -= 8;
-            }
-        }
-
-        public class Zergling : IUnit
-        {
-            public void Attack(Target target)
-            {
-                target.Health -= 5;
-            }
-        }
-
-        public class Mario
-        {
-            public int jumpAttack()
-            {
-                Console.WriteLine("Mamamia!");
-                return 3;
+            public void Attack(Target target){
+                target.Health -= this.mario.jumpAttack();
             }
         }
     }
